fd59589712b64cee52d0ba8b47b317331b74ec64
Robert Driskell <driskell.ba@gmail.com>
2019-04-12 12:48:09 -0500

    Got first image displaying

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,38 @@
 class Carousel {
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+        this.carouselImages = Array.from(this.carouselElement.querySelectorAll('img')).map(element => {
+            return new CarouselImages(element);
+        });
+    }
 
+    nextImage() {
+
+    }
+
+    previousImage() {
+
+    }
 }
 
-let carousel = document.querySelector();
+class CarouselImages {
+    constructor(carouselImage) {
+        this.carouselImage = carouselImage;
+        this.carouselData = this.carouselImage.dataset.vis;
+        console.log(this.carouselData);
+        if (this.carouselData == 'visible') {
+            this.carouselImage.style.display = 'flex';
+        }
+        else {
+            this.carouselImage.style.display = 'none';
+        }
+    }
+}
+
+let carousel = document.querySelectorAll('.carousel');
+carousel.forEach(carousel => {
+    new Carousel(carousel);
+});
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
